e526c0cc1c690f5b5999fd4c85473564f7cdc57d
Liuxiang400 <2090324417@qq.com>
2024-12-19 14:26:26 +0800

    Add files via upload

---
+++ mineswpeeper/cellitem.cpp
@@ -0,0 +1,90 @@
+#include "cellitem.h"
+#include <fielddata.h>
+
+int cellitem::_refCount = 0;
+
+QPixmap* cellitem::_blank = nullptr;
+QPixmap* cellitem::_explode = nullptr;
+QPixmap* cellitem::_flag = nullptr;
+QPixmap* cellitem::_initial = nullptr;
+QPixmap* cellitem::_question = nullptr;
+QPixmap* cellitem::_digits[9] = {nullptr};
+
+// 初始化
+
+cellitem::cellitem(int x,int y,enum cellstatus status , QGraphicsItem *parent)
+    :
+        QGraphicsPixmapItem(parent){
+    cx = x;
+    cy = y;
+    cell = status;
+    ++_refCount;//创建第一个cellItem时加载所有图片
+
+    if(_refCount == 1) {//引用计数，保证只new一次
+        _initial   =   new QPixmap("://images/initial.png");
+        _flag       =   new QPixmap("://images/flag.png");
+        _question   =   new QPixmap("://images/question.png");
+        _blank      =   new QPixmap("://images/blank.png");
+        _explode    =   new QPixmap("://images/explode.png");
+        // 要改成具体地址
+
+        QString prefix = "://images/";
+        QString num;
+        QString suffix = ".png";
+
+
+        for(int i=1;i<9;++i){
+            QString fileName;
+            fileName+= prefix;
+            fileName+=num.setNum(i);
+            fileName+=suffix;
+            _digits[i] = new QPixmap(fileName);
+        }
+    }
+    setPixmap (*_initial);
+    //qDebug()<<this<<endl;
+}
+
+cellitem::~cellitem(){
+    //释放所有cellItem后，释放图片
+    if(--_refCount == 0){
+        delete _initial   ;
+        delete _flag      ;
+        delete _question  ;
+        delete _blank     ;
+        delete _explode   ;
+        for(int i=0;i<9;++i){
+            delete _digits[i];
+        }
+    }
+}
+
+enum cellstatus cellitem::getStatus(){
+    return cell;
+}
+
+void cellitem::setStatus(enum cellstatus status,int digit){
+    cell =status;
+    switch(cell){
+    case INITIAL:
+        setPixmap(*_initial);
+        break;
+    case FLAG:
+        setPixmap(*_flag);
+        break;
+    case QUESTION:
+        setPixmap(*_question);
+        break;
+    case EXPLODE:
+        setPixmap(*_explode);
+        break;
+    case DIGIT:
+        setPixmap(*_digits[digit]);
+        break;
+    case BLANK:
+        setPixmap(*_blank);
+        break;
+    default:
+        break;
+    }
+}
---
+++ mineswpeeper/cellitem.h
@@ -0,0 +1,37 @@
+#ifndef CELLITEM_H
+#define CELLITEM_H
+//实现每个格子的功能
+
+#include <QPixmap>
+#include <QGraphicsPixmapItem>
+
+//每个小格子的行为
+enum cellstatus {INITIAL,EXPLODE,FLAG,QUESTION,BLANK,DIGIT};
+
+class cellitem : public QGraphicsPixmapItem {
+public:
+    cellitem(int x,int y,enum cellstatus status = INITIAL, QGraphicsItem *parent = Q_NULLPTR);
+    ~cellitem();
+
+    enum cellstatus getStatus(); //得到当前格子的状态
+    void setStatus(enum cellstatus status,int digit = -2); //设置格子状态
+
+    //static只初始化一次
+    static QPixmap* _initial;
+    static QPixmap* _explode;
+    static QPixmap* _flag;
+    static QPixmap* _question;
+    static QPixmap* _blank;
+    static QPixmap* _digits[9];
+    /*初始化图片*/
+
+    static int _refCount;
+
+protected:
+    int cx;
+    int cy; // 每个格子类有的坐标
+    enum cellstatus cell ;
+
+
+};
+#endif // CELLITEM_H
